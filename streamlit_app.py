acc9ddcba7a303400038c9e78f1e7c35baebac08
deeiye <121052257+deeiye@users.noreply.github.com>
2023-02-05 18:45:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The table contains:")
 streamlit.dataframe(my_data_rows)
 
+fruit_choice1 = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding ', fruit_choice1)
